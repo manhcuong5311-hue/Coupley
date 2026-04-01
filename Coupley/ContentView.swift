3e52367773aef6f52e40ec08ba2a9bba6819e93e
SamCorp <Manhcuong5311@gmail.com>
2026-04-01 17:35:11 +0700

    Initial Commit

---
+++ Coupley/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coupley
+//
+//  Created by Sam Manh Cuong on 1/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Coupley/CoupleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoupleyApp.swift
+//  Coupley
+//
+//  Created by Sam Manh Cuong on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CoupleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
